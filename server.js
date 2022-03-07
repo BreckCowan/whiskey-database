3a4891dd81e36c2841e563c844ca73a1b23b2f9d
Breck Cowan <breckcowan@gmail.com>
2022-03-06 19:12:09 -0600

    added whiskey schema

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+
+const app = express();
 var request = require("request");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = process.env.MONGO_URI;
 
-
-
 mongoose.connect(uri, { useUnifiedTopology: true });
 
-const notesSchema = {
-  title: String,
-  content: String,
+const whiskeySchema = {
+  name: String,
+  summary: String,
+  origin: String,
+  distiller: String,
+  proof: Number,
+  color: String,
+  mash_bill: Number,
+  founded: Number,
 };
 
-const Note = mongoose.model("Note", notesSchema);
+const Whiskey = mongoose.model("Whiskey", whiskeySchema);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-  let newNote = new Note({
-    title: req.body.title,
-    content: req.body.content,
+  let newWhiskey = new Whiskey({
+    name: req.body.name,
+    summary: req.body.summary,
+    origin: req.body.origin,
+    distiller: req.body.origin,
+    proof: req.body.proof,
+    color: req.body.color,
+    mash_bill: req.body.mash_bill,
+    founder: req.body.founder,
   });
-  newNote.save();
+  newWhiskey.save();
   res.redirect("/");
 });
 
@@ -38,9 +49,10 @@ async function main() {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
 
   try {
-    
-
     await client.connect();
+
+    await listDatabases(client);
+
   } catch (e) {
     console.error(e);
   } finally {
@@ -50,8 +62,34 @@ async function main() {
 
 main().catch(console.error);
 
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
 
+  console.log("Databases: ");
+  databasesList.databases.forEach((db) => {
+    console.log(`- ${db.name}`);
+  });
+}
 
+// const notesSchema = {
+//   title: String,
+//   content: String,
+// };
+
+// const Note = mongoose.model("Note", notesSchema);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/", (req, res) => {
+//   let newNote = new Note({
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
+//   newNote.save();
+//   res.redirect("/");
+// });
 
 app.listen(3000, () => {
   console.log("Server running on 3000");
